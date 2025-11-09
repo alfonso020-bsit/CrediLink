0d20fe1ca9b8c2509eb745d5f2cac457909a41d1
yuri <camonrazel01@gmail.com>
2025-11-09 11:00:18 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CrediLink',
+  webDir: 'www'
+};
+
+export default config;
